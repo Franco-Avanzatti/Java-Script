18bd6df6ab221f2843e3a4f83585b473b3ff5609
Franco <avanzatti5@gmail.com>
2024-09-22 17:27:55 -0300

    cargando producto al carrito

--- index.js
+++ index.js
@@ -1,84 +1,49 @@
-
-let saludo = alert(`Hola, Bienvenido a Tino!!`)
-
-const heladería = () => {
-    let continuar = true;
-    let productosVendidos = [
-        { tipo: 'Palito de helado', cantidad: 0, precio: 1000 },
-        { tipo: 'Pote de medio kilo', cantidad: 0, precio: 3000 },
-        { tipo: 'Pote de un kilo', cantidad: 0, precio: 5500 }
-    ];
-
-    //Eleccion de helado
-    while (continuar) {
-        let opcion = prompt("Elige una opción:\n\n1. Palito de helado\n2. Pote de medio kilo\n3. Pote de un kilo\n\nEscribe 1, 2 o 3 para elegir:");
-        
-        const dinero = parseFloat(prompt("¿Cuánto dinero tienes?"));
-
-        
-        if (isNaN(dinero) || dinero < 1000) {
-            alert("Lo siento, no tienes suficiente dinero para comprar ningún helado.");
-            let respuesta = prompt("¿Deseas intentarlo de nuevo? (si/no)");
-            continuar = respuesta.toLowerCase() === 'si' ? true : false;
-            continue; 
-        }
-
-        switch(opcion) {
-            case '1':
-                if (dinero >= productosVendidos[0].precio) {
-                    alert("Usted eligió un palito de helado.");
-                    productosVendidos[0].cantidad++;
-                } else {
-                    alert("Lo siento, no tienes suficiente dinero para un palito de helado.");
-                }
-                break;
-            case '2':
-                if (dinero >= productosVendidos[1].precio) {
-                    alert("Usted eligió un pote de medio kilo.");
-                    productosVendidos[1].cantidad++;
-                } else {
-                    alert("Lo siento, no tienes suficiente dinero para un pote de medio kilo.");
-                }
-                break;
-            case '3':
-                if (dinero >= productosVendidos[2].precio) {
-                    alert("Usted eligió un pote de un kilo.");
-                    productosVendidos[2].cantidad++;
-                } else {
-                    alert("Lo siento, no tienes suficiente dinero para un pote de un kilo.");
-                }
-                break;
-            default:
-                alert("Opción no válida, por favor elija de nuevo.");
-                continue; 
-        }
-
-        let respuesta = prompt("Gracias por su compra, ¿desea algo más? (si/no)");
-        continuar = respuesta.toLowerCase() === 'si' ? true : false;
+const heladosTino = [
+    {
+        id:"heladoDePalito",
+        titulo:"Helado de Palito",
+        precio:1000,
+    },
+    {
+        id:"poteDeMedioKilo",
+        titulo:"Pote de medio kilo",
+        precio:3000,
+    },
+    {
+        id:"poteDeKilo",
+        titulo:"Pote de kilo",
+        precio:5500
     }
+]
 
-    alert("Gracias por su compra, ¡vuelva pronto!");
-    console.log("      *** VENTAS ***   ");
+const contenedorProductos = document.querySelector("#contenedor-productos");
+
+function cargarProductos () {
     
-    productosVendidos.forEach(producto => {
-        console.log(`${producto.tipo}: ${producto.cantidad} vendidos`);
-    });
+    cargarProductos.forEach(producto => {
 
-    // Calcular y mostrar los totales
-    const totalVentas = calcularTotales(productosVendidos);
-    console.log(`Helados vendidos: ${totalVentas.totalCantidad}`);
-    console.log(`Total de ventas: $${totalVentas.totalDinero}`);
-};
+        const div =document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML= `
+        <img src="${producto.imagen}" class="producto-imagen card-img-top" alt="${producto.titulo}">
+          <div class="producto-detalle card-body">
+            <h5 class="prodcuto-titulo card-title">${producto.titulo}</h5>
+            <p class="producto-precio card-text">$${producto.precio}</p>
+            <a href="#" class="producto-agregar btn btn-primary" id="${producto.id}" >Agregar al carrito</a>
+          </div>
 
-// Total de ventas y cantidad total de productos vendidos
-const calcularTotales = (productos) => {
-    const totalDinero = productos.reduce((acc, producto) => acc + (producto.cantidad * producto.precio), 0);
-    const totalCantidad = productos.reduce((acc, producto) => acc + producto.cantidad, 0);
-    
-    return { totalDinero, totalCantidad };
-};
+        `;
 
-heladería();
+        contenedorProductos.append(div);
+
+        
+
+    })
+
+}
+
+cargarProductos();
+
 
   
   
